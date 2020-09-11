4e57d21efac36023c8c4a74a9cfcda2b25fded77
PC <amolujagare@gmail.com>
2020-09-11 18:59:21 +0530

    set

--- src/WrapperClassDemos/VariablesDemo.java
+++ src/WrapperClassDemos/VariablesDemo.java
@@ -14,5 +14,24 @@ public class VariablesDemo {
         char c = '#';
 
 
+        int i = 10;
+
+        Integer ii = new Integer(10);
+        Integer ii1 = new Integer(i); // wrapping or boxing
+
+        Integer ii2 = 10;
+        Integer ii3 = i; // auto wrapping or auto boxing
+
+
+        int a1 = ii.intValue();
+
+        System.out.println(a1);
+
+        int a2 = ii;
+        System.out.println(a2);
+
+        for(Integer j =0 ;j<10 ;j++)
+            System.out.println(j);
+
     }
 }
